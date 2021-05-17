2209f99cac33da78c7567cfd68a4204a0edbd839
Konstantin Kostin <newuser9929@gmail.com>
2021-05-17 20:57:13 +0300

    driver: command queue API tests

--- driver/src/runtime/icd/CLCommandQueue.h
+++ driver/src/runtime/icd/CLCommandQueue.h
@@ -5,24 +5,29 @@
 
 #include "IcdDispatchTable.h"
 #include "runtime/command/Command.h"
+#include "runtime/icd/CLContext.h"
 
 class Command;
 
 class CLCommandQueue {
    public:
-    explicit CLCommandQueue(IcdDispatchTable* dispatchTable)
-        : dispatchTable(dispatchTable) {}
+    explicit CLCommandQueue(IcdDispatchTable* dispatchTable,
+                            CLContext* context,
+                            cl_command_queue_properties properties);
+
+    virtual ~CLCommandQueue();
 
     IcdDispatchTable* const dispatchTable;
-
-    bool profilingEnabled = false;
-    bool outOfOrderMode = false;
+    CLContext* const context;
+    cl_command_queue_properties properties;
 
     unsigned int referenceCount = 1;
 
     void enqueue(const std::shared_ptr<const Command>& command);
 
     void flush();
+
+    size_t size();
 
    private:
     std::queue<std::shared_ptr<const Command>> commands =
--- driver/test/unit/unit-test-common/test-commons.h
+++ driver/test/unit/unit-test-common/test-commons.h
@@ -5,9 +5,15 @@
 #include "runtime/icd/CLPlatformId.hpp"
 #include "runtime/icd/CLDeviceId.hpp"
 #include "runtime/icd/CLContext.h"
+#include "runtime/icd/CLCommandQueue.h"
 
 namespace test {
+struct DummyCommand : public Command {
+    void execute() const override{};
+};
+
 cl_platform_id getPlatform();
 cl_device_id getDevice();
 cl_context getContext();
+cl_command_queue getCommandQueue();
 }  // namespace test
